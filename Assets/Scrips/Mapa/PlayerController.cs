9b52a008cc3967e0553d9421d02bc81ad9266ec0
Marta Jones <martajon10@gmail.com>
2024-03-27 14:28:25 +0100

    movimiento player tablero

--- Assets/Scrips/Mapa/PlayerController.cs
+++ Assets/Scrips/Mapa/PlayerController.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 
 public class PersonajeController : MonoBehaviour
 {
+    public Camera camara;
+    private Vector3 offset;
+
     private Rigidbody rb;
     private PlatformController plataformaActual;
+
     private Vector3 targetPosition; // Posición a la que se moverá el personaje
     public float velocidadMovimiento = 10f; 
-    private bool enMovimiento = false;
+    private bool enMovimiento;
     public float distanciaMinima = 1f; // Distancia mínima para llegar a la posición deseada
+
     private bool isJumping = false;
     public float jumpHeight = 2f;
     public float jumpSpeed = 4f;
@@ -19,38 +24,42 @@ public class PersonajeController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true; // Para evitar que se caiga de frente
+
+        offset = camara.transform.position - transform.position;
+
+        enMovimiento = false;
     }
 
     void Update(){
+
         if(!enMovimiento && Input.GetKeyDown(KeyCode.RightArrow) && plataformaActual != null){
-            // Debug.Log("El personaje se moverá a la siguiente plataforma.");
             // Posición a la que se moverá el personaje
             targetPosition = plataformaActual.ObtenerSiguientePlataforma().position;
             enMovimiento = true;
-        }
+
+            RotacionPersonaje();
+        } 
         if(enMovimiento){
              // La velocidad a la que se moverá el personaje
-            float step = velocidadMovimiento * Time.fixedDeltaTime; 
             StartCoroutine(Jump());
-            //rb.MovePosition(Vector3.MoveTowards(transform.position, targetPosition, step));
-            // Debug.Log("El personaje se está moviendo.");
             // Si ya llegamos a la posición deseada
             float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
-            //Debug.Log("Distancia al objetivo: " + distanceToTarget);
             if(distanceToTarget < distanciaMinima)
             {
-                transform.position = targetPosition;
                 enMovimiento = false;
                 plataformaActual = null;
                 Debug.Log("El personaje ha llegado a la posición deseada.");
             }
         } 
+
+        camara.transform.position = transform.position + offset;
+    
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Platform") && plataformaActual == null)
+        if (collision.gameObject.CompareTag("Platform"))
         {
             Debug.Log("El personaje está en contacto con la plataforma.");
             // Obtenemos la plataforma con la que colisionamos
@@ -62,7 +71,6 @@ public class PersonajeController : MonoBehaviour
     {
         if (!isJumping)
         {
-        
             isJumping = true;
 
             Vector3 startPosition = transform.position;
@@ -77,8 +85,26 @@ public class PersonajeController : MonoBehaviour
                 yield return null;
             }
 
-            transform.position = this.targetPosition;
+            transform.position = targetPosition;
             isJumping = false;
         }
+    }
+
+    void RotacionPersonaje()
+    {
+        float umbral = 20.0f; // Ajusta este valor según sea necesario
+        if(targetPosition.x > transform.position.x + umbral)
+        {
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+        else if(targetPosition.x < transform.position.x - umbral)
+        {
+            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+        } else if(targetPosition.z > transform.position.z + umbral){
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        } else {
+            transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        }
+
     }
 }
